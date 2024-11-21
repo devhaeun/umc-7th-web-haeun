ea550d20bc15a1c0d26d8966966dab5ea6b922e0
devhaeun <lala819010@gmail.com>
2024-11-22 03:46:10 +0900

    week8 미션1 GET/POST까지 해결

---
+++ week8/umc-todo/src/components/CenterDiv.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const CenterDiv = styled.div`
+    min-width: 500px;
+    // background-color: pink;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export default CenterDiv;
---
+++ week8/umc-todo/src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+const NavBar = () => {
+    return(
+        <>
+        <h1>⚡UMC TODO LIST⚡</h1>
+        </>
+    );
+}
+
+export default NavBar;
